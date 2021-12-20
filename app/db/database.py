73f7ea5b3da9899947d4e654270850e761211fb6
Óscar García <oscar.gpoblacion@uah.es>
2021-12-20 19:30:49 +0000

    Fix indentation

--- app/db/database.py
+++ app/db/database.py
@@ -9,7 +9,7 @@ def get_db():
     db_name = os.environ['DB_NAME']
     db_connection_name = os.environ['CLOUD_SQL_CONNECTION_NAME']
 
-	if 'db' not in g and db_connection_name:
+    if 'db' not in g and db_connection_name:
         # If there is a db_connection_name then we assume that we are
         # connecting to a unix socket, whose name is google-specific.
         unix_socket = '/cloudsql/{}'.format(db_connection_name)
