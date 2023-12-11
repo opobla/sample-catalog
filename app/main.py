519fad958b47edd35ac07ba7318fc9d24e6e7db4
Óscar García <oscar.gpoblacion@uah.es>
2023-12-11 10:11:26 +0000

    Generate fake responses via Faker

--- app/main.py
+++ app/main.py
@@ -5,34 +5,34 @@ app = Flask(__name__)
 
 @app.route('/product', methods=['GET', 'POST'])
 def list_all_products():
-	'''This view manages the CRUD of products'''
-	print("Hola mundo")
-	if request.method == 'GET':
-		response = get_products()
-		return jsonify(response)
-	
-	if request.method == 'POST':
-		data = request.get_json()
-		create_product(
-			data['sku'],
-			data['title'],
-			data['long_description'],
-			data['price_euro'])
-		return jsonify({ "status": "ok"})
+    '''This view manages the CRUD of products'''
+    if request.method == 'GET':
+        response = get_products()
+        return jsonify(response)
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        data['sku'] = data['sku'] if 'sku' in data else None
+        create_product(
+            data['sku'],
+            data['title'],
+            data['long_description'],
+            data['price_euro'])
+        return jsonify({ "status": "ok"})
 
 
 @app.route('/hello')
 def hello_world():
-	message = "Hola Mundo, soy Python! Ahora con CloudBuild y hablando JSON"
-	response = {
-		"message": message,
-		"length": len(message)
-	}
-	return jsonify(response)
+    message = "Hola Mundo, soy Python! Ahora con CloudBuild y hablando JSON"
+    response = {
+        "message": message,
+        "length": len(message)
+    }
+    return jsonify(response)
 
 @app.route('/bye')
 def bye_world():
-	return ("Adios mundo cruel")
+    return ("Adios mundo cruel")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
